4808d8aa9ba10a9cdfd55ad1521b6dbcadf4f113
nathaniel nosa <nathanielnarth@gmail.com>
2024-03-21 09:25:38 +0100

    restrict login user from login & register page

--- src/routers/RouterMain.jsx
+++ src/routers/RouterMain.jsx
@@ -2,7 +2,8 @@ import {
   createBrowserRouter,
   RouterProvider,
   Route,
-  createRoutesFromElements
+  createRoutesFromElements,
+  Navigate
 } from "react-router-dom"
 
 import App from "../App"
@@ -16,15 +17,17 @@ import Login from "../pages/Login"
 import Cart from "../pages/Cart"
 
 const RouterMain = () => {
+  const user = true
   const routers = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<App />}>
         <Route index element={<Home />} />
         <Route path="/musics" element={<Music />} />
         <Route path="/about" element={<About />} />
+        <Route path="/music/:category" element={<Detail />} />
         <Route path="/music/:id" element={<Detail />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={user ? <Navigate to={"/"} /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to={"/"} /> : <Login />} />
         <Route path="/cart" element={<Cart />} />
 
       </Route>
